7c700110656ee7d07d608123c72f000f703a6f7b
Four Pixels Studio <121856163+fourpixels-studio@users.noreply.github.com>
2024-08-24 20:40:52 +0300

    Added events

--- frontend/views.py
+++ frontend/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from events.models import Event
 
 
 def index(request):
     context = {
         "title_tag": "Home",
+        "events": Event.objects.order_by("-pk"),
     }
     return render(request, "index.html", context)
