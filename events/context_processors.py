4ef58ff1f97aceb3aa1236f6ec9660d8605d92b9
Four Pixels Studio <121856163+fourpixels-studio@users.noreply.github.com>
2024-08-25 20:06:03 +0300

    First commit 🚀

---
+++ events/context_processors.py
@@ -0,0 +1,35 @@
+from .models import Event
+from django.contrib import messages
+from django.db.models import Q
+
+
+def search_function(request):
+    search_context = {}
+    if request.GET:
+        query = request.GET.get("q")
+        if query:
+            name_results = Q(name__icontains=query)
+            location_results = Q(location__icontains=query)
+            venue_results = Q(venue__icontains=query)
+            event_keywords_results = Q(event_keywords__icontains=query)
+            event_description_results = Q(event_description__icontains=query)
+
+            results = Event.objects.filter(
+                name_results | location_results |
+                venue_results | event_keywords_results |
+                event_description_results
+            ).distinct()
+
+        else:
+            messages.info(request, "No results")
+            results = []
+
+        num_results = results.count()
+
+        search_context = {
+            "events": results,
+            "query": query,
+            "num_results": num_results,
+        }
+
+    return search_context
