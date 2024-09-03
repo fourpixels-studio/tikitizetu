eb650681939defc546222d68e264c20ab50ebc01
Four Pixels Studio <121856163+fourpixels-studio@users.noreply.github.com>
2024-09-04 00:15:07 +0300

    Refactored view ticket

--- tickets/urls.py
+++ tickets/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from .views import view_ticket, new_ticket
 
 urlpatterns = [
-    path("<int:pk>/<str:ticket_number>",
-         view_ticket, name="view_ticket"),
+    path("<slug:slug>/<str:ticket_number>/<int:pk>/", view_ticket, name="view_ticket"),
     path("new-ticket/", new_ticket, name="new_ticket"),
 ]
