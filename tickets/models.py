d7ace9d4755f49ccd7f5179bdd290a4823bcfef5
Four Pixels Studio <121856163+fourpixels-studio@users.noreply.github.com>
2024-09-04 00:15:57 +0300

    Removed num_tickets

--- tickets/models.py
+++ tickets/models.py
@@ -10,12 +10,11 @@ class Ticket(models.Model):
     last_name = models.CharField(max_length=100)
     email = models.EmailField(blank=True, null=True)
     phone_number = models.CharField(max_length=20, null=True, blank=True)
+    ticket_type = models.CharField(max_length=30, blank=True, null=True)
     amount = models.CharField(max_length=5)
-    num_tickets = models.CharField(max_length=2, blank=True, null=True)
-    qr = models.FileField(upload_to='qrcodes/', max_length=100)
+    qr = models.FileField(upload_to='qrcodes/', blank=True, null=True)
     pdf_ticket = models.FileField(
         upload_to='tickets/', max_length=100, null=True, blank=True)
-    scan_count = models.PositiveIntegerField(default=0)
 
     @property
     def get_event(self):
@@ -28,7 +27,8 @@ class Ticket(models.Model):
         return reverse("view_ticket", kwargs={
             "event_name": self.event.slug,
             "ticket_number": self.ticket_number,
+            "event_id": self.event.pk,
         })
 
     def __str__(self):
-        return f"{self.first_name} {self.last_name}'s ticket | {self.ticket_number} | Scanned {self.scan_count} times | Event: {self.event}"
+        return f"{self.first_name} {self.last_name}'s ticket | {self.ticket_number} | Event: {self.event}"
