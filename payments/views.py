7f45ab269d8953eb561e8bddfddc2598f83b7c85
Four Pixels Studio <121856163+fourpixels-studio@users.noreply.github.com>
2024-09-07 23:12:33 +0300

    Removed extra details

--- payments/views.py
+++ payments/views.py
@@ -1,14 +1,4 @@
-import os
 from .card_validation import validate_card_number
-BANK_ACCOUNT_NUMBER = os.environ.get('BANK_ACCOUNT_NUMBER')
-BANK_CODE = 57
-BANK_NAME = 'I&M Bank Limited'
-ACCOUNT_NAME = 'NAVINE ERIC'
-SWIFT_CODE = 'IMBLKENA'
-BRANCH_NAME = 'Nairobi Head Office'
-CURRENCY = 'KES'
-num = 4539148803436467
-
 
 def process_bank_payment(card_number, cvc_number, expiry_date, amount):
     if validate_card_number(card_number):
