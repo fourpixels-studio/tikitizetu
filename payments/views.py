e9d742b939370abf07309acdb11be27c993a13c3
Four Pixels Studio <121856163+fourpixels-studio@users.noreply.github.com>
2024-09-07 23:21:01 +0300

    Added payment_failed and process_payment

--- payments/views.py
+++ payments/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import render
 from .card_validation import validate_card_number
 
 def process_bank_payment(card_number, cvc_number, expiry_date, amount):
@@ -5,3 +6,11 @@ def process_bank_payment(card_number, cvc_number, expiry_date, amount):
         return True
     else:
         return False
+
+
+def payment_failed(request):
+    return render(request, "payment_failed.html", {"title_tag": "Payment Failed."})
+
+
+def process_payment(request):
+    return render(request, "process_payment.html", {"title_tag": "Process Payment."})
