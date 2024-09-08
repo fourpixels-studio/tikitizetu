2b6674efc760858a09bc1b15852cd215fdc1f0ef
Four Pixels Studio <121856163+fourpixels-studio@users.noreply.github.com>
2024-09-08 19:29:44 +0300

    Added i&m callback urls

--- payments/urls.py
+++ payments/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import payment_failed, process_payment
+from .im_payments import im_payment_callback, im_payment_success, im_payment_failed
 
 urlpatterns = [
-    path("failed/", payment_failed, name="payment_failed"),
-    path("process-payment/", process_payment, name="process_payment"),
+    # I&M Bank
+    path("im/payment-callback/", im_payment_callback, name="im_payment_callback"),
+    path("im/payment-success/", im_payment_success, name="im_payment_success"),
+    path("im/payment-failed/", im_payment_failed, name="im_payment_failed"),
 ]
