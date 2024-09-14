640e4766779dcf3d66da2b107d92ba669bc820a2
Four Pixels Studio <121856163+fourpixels-studio@users.noreply.github.com>
2024-09-14 18:36:34 +0300

    Added payment-failed

--- tickets/urls.py
+++ tickets/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import view_ticket, new_ticket
+from .views import view_ticket, purchase_ticket
+from payments.views import pesapal_payment_failed
 
 urlpatterns = [
     path("<slug:slug>/<str:ticket_number>/<int:pk>/", view_ticket, name="view_ticket"),
-    path("new-ticket/", new_ticket, name="new_ticket"),
+    path("purchase/", purchase_ticket, name="purchase_ticket"),
+    path("payment-failed/<str:ticket_number>/", pesapal_payment_failed, name="pesapal_payment_failed"),
 ]
