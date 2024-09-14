0c2fb71c69d6dd39c3fd1e3e56c3c46a1bf31970
Four Pixels Studio <121856163+fourpixels-studio@users.noreply.github.com>
2024-09-14 18:33:23 +0300

    Pesapal second commit

--- payments/urls.py
+++ payments/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
-from .im_payments import im_payment_callback, im_payment_success, im_payment_failed
+from .pesapal_payments import pesapal_payment_ipn, pesapal_payment_callback
 
 urlpatterns = [
-    # I&M Bank
-    path("im/payment-callback/", im_payment_callback, name="im_payment_callback"),
-    path("im/payment-success/", im_payment_success, name="im_payment_success"),
-    path("im/payment-failed/", im_payment_failed, name="im_payment_failed"),
+    path("pesapal/payment-callback/", pesapal_payment_callback, name="pesapal_payment_callback"),
+    path('pesapal/payment-ipn/', pesapal_payment_ipn, name='pesapal_payment_ipn')
 ]
