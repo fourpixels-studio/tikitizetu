6a0d9eaa0b4e4f631db6a28ea6c80d0fe1fc5855
Four Pixels Studio <121856163+fourpixels-studio@users.noreply.github.com>
2024-09-15 22:29:11 +0300

    Second commit

--- payments/safaricom_payments.py
+++ payments/safaricom_payments.py
@@ -1,60 +1,94 @@
+from django.views.decorators.debug import sensitive_variables
 import requests
 import base64
 from datetime import datetime
-import os
+from django.conf import settings
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse
+from django.shortcuts import redirect
+from tickets.models import Ticket
 
 
-def generate_password(shortcode, passkey, timestamp):
-    data_to_encode = shortcode + passkey + timestamp
-    encoded_string = base64.b64encode(data_to_encode.encode())
-    return encoded_string.decode('utf-8')
+@sensitive_variables('PESAPAL_CONSUMER_KEY', 'PESAPAL_CONSUMER_SECRET', 'SAFARICOM_PASSKEY')
+class Safaricom:
+    def __init__(self):
+        self.base_url = 'https://sandbox.safaricom.co.ke/'
+        self.InitiatorName = 'testapi'
+        self.InitiatorPassword = settings.SAFARICOM_INITIATOR_PASSWORD
+        self.shortcode = settings.SAFARICOM_SHORTCODE
+        self.passkey = settings.SAFARICOM_PASSKEY
 
+    def get_access_token(self):
+        endpoint = "oauth/v1/generate?grant_type=client_credentials"
+        response = requests.get(
+            self.base_url+endpoint,
+            auth=(
+                settings.SAFARICOM_CONSUMER_KEY,
+                settings.SAFARICOM_CONSUMER_SECRET
+            )
+        )
+        return response.json()['access_token']
 
-def get_mpesa_access_token(consumer_key, consumer_secret):
-    api_url = "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials"
-    r = requests.get(api_url, auth=(consumer_key, consumer_secret))
-    return r.json()['access_token']
+    def generate_password(self, timestamp):
+        data_to_encode = self.shortcode + self.passkey + timestamp
+        encoded_string = base64.b64encode(data_to_encode.encode())
+        return encoded_string.decode('utf-8')
 
+    def initiate_stk_push(self, amount, PhoneNumber, event_slug, ticket_number, event_pk):
+        endpoint = "mpesa/stkpush/v1/processrequest"
+        timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
+        CallBackURL = f'https://www.tikitizetu.com/ticket/{event_slug}/{ticket_number}/{event_pk}/'
 
-def process_payment(phone_number, amount):
-    consumer_key = os.environ.get('MPESA_CONSUMER_KEY')
-    consumer_secret = os.environ.get('MPESA_CONSUMER_SECRET')
-    shortcode = os.environ.get('MPESA_SHORTCODE')
-    passkey = os.environ.get('MPESA_PASSKEY')
+        headers = {
+            'Authorization': f'Bearer {self.get_access_token()}',
+            'Content-Type': 'application/json',
+        }
 
-    timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
-    password = generate_password(shortcode, passkey, timestamp)
-    access_token = get_mpesa_access_token(consumer_key, consumer_secret)
-
-    headers = {
-        'Authorization': f'Bearer {access_token}',
-        'Content-Type': 'application/json',
-    }
-
-    payload = {
-        "BusinessShortCode": shortcode,
-        "Password": password,
-        "Timestamp": timestamp,
-        "TransactionType": "CustomerPayBillOnline",
-        "Amount": amount,
-        "PartyA": phone_number,
-        "PartyB": shortcode,
-        "PhoneNumber": phone_number,
-        "CallBackURL": "https://tikitizetu.com/mpesa-callback/",
-        "AccountReference": "Ticket Purchase",
-        "TransactionDesc": "Payment for event ticket",
-    }
-
-    api_url = "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest"
-    response = requests.post(api_url, json=payload, headers=headers)
-
-    if response.status_code == 200:
-        response_data = response.json()
-        if response_data['ResponseCode'] == "0":
-            return True
+        payload = {
+            "BusinessShortCode": "174379",
+            "Password": self.generate_password(timestamp),
+            "Timestamp": timestamp,
+            "TransactionType": "CustomerPayBillOnline",
+            "Amount": amount,
+            "PartyA": PhoneNumber,
+            "PartyB": "174379",
+            "PhoneNumber": PhoneNumber,
+            "CallBackURL": CallBackURL,
+            "AccountReference": "Tikiti Zetu",
+            "TransactionDesc": "Tikiti Zetu"
+        }
+        response = requests.post(
+            self.base_url+endpoint,
+            json=payload, headers=headers
+        )
+        if response.status_code == 200:
+            response_data = response.json()
+            if response_data['ResponseCode'] == "0":
+                print(response.json())
+                return True
+            else:
+                print(f"Error: {response_data['ResponseDescription']}")
+                return False
         else:
-            print(f"Error: {response_data['ResponseDescription']}")
+            print(f"HTTP Error: {response.status_code}")
+            print(response.json())
             return False
-    else:
-        print(f"HTTP Error: {response.status_code}")
-        return False
+
+
+@csrf_exempt
+def safaricom_payment_callback(request):
+    ResponseCode = request.GET.get('ResponseCode')
+    MpesaReceiptNumber = request.GET.get('MpesaReceiptNumber')
+
+    if ResponseCode == '0':
+        try:
+            ticket = Ticket.objects.get(mpesa_code=MpesaReceiptNumber)
+            ticket.paid = True
+            ticket.status = "Paid"
+            ticket.save()
+            return redirect('view_ticket', ticket.event.slug, ticket.ticket_number, ticket.event.pk)
+        except Ticket.DoesNotExist:
+            ticket.status = 'Ticket not found'
+            ticket.save()
+            return redirect('cart', slug=ticket.event.slug, pk=ticket.event.pk)
+    return HttpResponse("Payment failed", status=404)
