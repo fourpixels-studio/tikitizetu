722ec1aa9f6884e13e89862736b92a9033a72b32
Four Pixels Studio <121856163+fourpixels-studio@users.noreply.github.com>
2024-09-18 21:43:38 +0300

    Removed payment failed

--- tickets/urls.py
+++ tickets/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
 from .views import view_ticket, purchase_ticket
-from payments.views import pesapal_payment_failed
 
 urlpatterns = [
     path("<slug:slug>/<str:ticket_number>/<int:pk>/", view_ticket, name="view_ticket"),
     path("purchase/", purchase_ticket, name="purchase_ticket"),
-    path("payment-failed/<str:ticket_number>/", pesapal_payment_failed, name="pesapal_payment_failed"),
 ]
