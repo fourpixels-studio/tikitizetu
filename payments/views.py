295b03d2585ac718986a72d20de8fe1b3cf51b77
Four Pixels Studio <121856163+fourpixels-studio@users.noreply.github.com>
2024-09-21 14:14:33 +0300

    Removed if else statements on success view

--- payments/views.py
+++ payments/views.py
@@ -15,17 +15,11 @@ def payment_failed(request, ticket_number):
 
 def payment_success(request, ticket_number):
     ticket = Ticket.objects.get(ticket_number=ticket_number)
-    if ticket.qr:
-        pass
-    else:
-        ticket_url = request.build_absolute_uri(reverse(
-            'view_ticket', args=[ticket.event.slug, ticket.ticket_number, ticket.event.pk]))
-        generate_qr(ticket_url, ticket)
-        ticket.save()
-    if ticket.pdf_ticket:
-        pass
-    else:
-        generate_pdf(ticket)
-        send_ticket_email(ticket)
-        ticket.save()
+    ticket_url = request.build_absolute_uri(reverse(
+        'view_ticket', args=[ticket.event.slug, ticket.ticket_number, ticket.event.pk]))
+    generate_qr(ticket_url, ticket)
+    ticket.save()
+    generate_pdf(ticket)
+    ticket.save()
+    send_ticket_email(ticket)
     return redirect('view_ticket', ticket.event.slug, ticket.ticket_number, ticket.event.pk)
