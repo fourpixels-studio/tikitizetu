dd8fb817f87c4f53ace442eb67bdb7bd653e1c1d
Four Pixels Studio <121856163+fourpixels-studio@users.noreply.github.com>
2024-09-22 16:04:50 +0300

    Updated pesapal callback url

--- payments/urls.py
+++ payments/urls.py
@@ -7,14 +7,15 @@ from .views import payment_failed, payment_success
 urlpatterns = [
     # pesapal urls
     path("pesapal/payment-callback/", pesapal_payment_callback, name="pesapal_payment_callback"),
-    path('pesapal/payment-ipn/', pesapal_payment_ipn, name='pesapal_payment_ipn'),
+    path('pesapal/ipn/', pesapal_payment_ipn, name='pesapal_payment_ipn'),
+    
     # safaricom urls
     path('s/processing-payment/<str:ticket_number>/', safaricom_processing_payment, name='safaricom_processing_payment'),
     path("s/payment-confirmation/", safaricom_payment_callback, name="safaricom_payment_callback"),
     path("s/payment-validation/", safaricom_payment_validation, name="safaricom_payment_validation"),
     path("s/check-payment-status/<str:ticket_number>/", safaricom_check_payment_status, name="safaricom_check_payment_status"),
+    
     # general urls
     path("payment-failed/<str:ticket_number>/", payment_failed, name="payment_failed"),
     path("payment-success/<str:ticket_number>/", payment_success, name="payment_success"),
-
 ]
