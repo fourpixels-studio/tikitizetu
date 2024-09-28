f661012b1014590e14ed95f3278008911c25fff1
Four Pixels Studio <121856163+fourpixels-studio@users.noreply.github.com>
2024-09-28 23:09:34 +0300

    Added event categories

--- events/views.py
+++ events/views.py
@@ -1,11 +1,21 @@
+from .models import Event, EventCategory
 from django.shortcuts import render, get_object_or_404
-from .models import Event
 
 
 def events_list(request):
+    category = request.GET.get('category')
+    if category is not None:
+        events = Event.objects.filter(category__slug=category).order_by("-pk")
+    else:
+        events = Event.objects.order_by("-pk")
+
+    active_category = request.GET.get('category', None)
     context = {
         "title_tag": "Events",
-        "events": Event.objects.order_by("-pk"),
+        'category_name': "All",
+        "events": events,
+        "active_category": active_category,
+        "categories": EventCategory.objects.all(),
     }
     return render(request, "events_list.html", context)
 
