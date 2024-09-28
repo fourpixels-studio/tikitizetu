07a641a25e74135b2b46bcff7e74296114df19b8
Four Pixels Studio <121856163+fourpixels-studio@users.noreply.github.com>
2024-09-28 23:10:39 +0300

    Added event categories

--- frontend/views.py
+++ frontend/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render
-from events.models import Event
+from events.models import Event, EventCategory
 
 
 def index(request):
+    category = request.GET.get('category')
+    if category is not None:
+        events = Event.objects.filter(category__slug=category).order_by("-pk")
+    else:
+        events = Event.objects.order_by("-pk")
+    active_category = request.GET.get('category', None)
+
     context = {
         "title_tag": "Home",
-        "events": Event.objects.order_by("-pk"),
+        "events": events,
+        'category_name': "All",
+        "active_category": active_category,
+        "categories": EventCategory.objects.all(),
     }
     return render(request, "index.html", context)
 
