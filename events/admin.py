0d6ca87792faf5de202a64f26f5559bdc03be831
Four Pixels Studio <121856163+fourpixels-studio@users.noreply.github.com>
2024-09-28 23:09:04 +0300

    Added EventCategory

--- events/admin.py
+++ events/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Event, TicketCategory
+from .models import Event, TicketCategory, EventCategory
 admin.site.register(Event)
+admin.site.register(EventCategory)
 admin.site.register(TicketCategory)
