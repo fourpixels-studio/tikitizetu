07da964cb5d935283cef3a15e19acd456611a0ec
Four Pixels Studio <121856163+fourpixels-studio@users.noreply.github.com>
2025-02-11 15:58:14 +0300

    Added tickets

--- tickets/urls.py
+++ tickets/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import view_ticket, purchase_ticket
+from .views import view_ticket, purchase_ticket, tickets
 
 urlpatterns = [
-    path("<slug:slug>/<str:ticket_number>/<int:pk>/", view_ticket, name="view_ticket"),
+    path("tickets/", tickets, name="tickets"),
     path("purchase/", purchase_ticket, name="purchase_ticket"),
+    path("<slug:slug>/<str:ticket_number>/<int:pk>/", view_ticket, name="view_ticket"),
 ]
