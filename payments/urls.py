442613567a388cc7df07a48c98253a5cae4192ad
Four Pixels Studio <121856163+fourpixels-studio@users.noreply.github.com>
2025-02-11 15:51:04 +0300

    Added check_transaction_status

--- payments/urls.py
+++ payments/urls.py
@@ -1,21 +1,22 @@
 from django.urls import path
-from .pesapal_payments import pesapal_payment_ipn, pesapal_payment_callback
-from .safaricom_payments import safaricom_payment_callback, safaricom_payment_validation, safaricom_processing_payment, safaricom_check_payment_status
 from .views import payment_failed, payment_success
+from .pesapal_payments import pesapal_payment_ipn, pesapal_payment_callback, check_transaction_status
+from .safaricom_payments import safaricom_payment_callback, safaricom_payment_validation, safaricom_processing_payment, safaricom_check_payment_status
 
 
 urlpatterns = [
     # pesapal urls
-    path("pesapal/payment-callback/", pesapal_payment_callback, name="pesapal_payment_callback"),
     path('pesapal/ipn/', pesapal_payment_ipn, name='pesapal_payment_ipn'),
+    path("pesapal/payment-callback/", pesapal_payment_callback, name="pesapal_payment_callback"),
     
     # safaricom urls
-    path('s/processing-payment/<str:ticket_number>/', safaricom_processing_payment, name='safaricom_processing_payment'),
     path("s/payment-callback/", safaricom_payment_callback, name="safaricom_payment_callback"),
     path("s/payment-validation/", safaricom_payment_validation, name="safaricom_payment_validation"),
+    path('s/processing-payment/<str:ticket_number>/', safaricom_processing_payment, name='safaricom_processing_payment'),
     path("s/check-payment-status/<str:ticket_number>/", safaricom_check_payment_status, name="safaricom_check_payment_status"),
     
     # general urls
     path("payment-failed/<str:ticket_number>/", payment_failed, name="payment_failed"),
     path("payment-success/<str:ticket_number>/", payment_success, name="payment_success"),
+    path("transaction-data/<str:tracking_id>/", check_transaction_status, name="check_transaction_status"),
 ]
