9bab99bd50664e21e0335039d0b4eeb5bdf8d4ed
Four Pixels Studio <121856163+fourpixels-studio@users.noreply.github.com>
2025-03-07 22:48:49 +0300

    Added submit_testimonial

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import index, contact
+from .views import index, contact, submit_testimonial
 
 urlpatterns = [
     path("", index, name="index"),
     path("contact/", contact, name="contact"),
+    path('submit-testimonial/', submit_testimonial, name='submit_testimonial'),
 ]
