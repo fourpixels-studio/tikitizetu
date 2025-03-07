d63475b619f9d9ee3607147c631b1e3b38997605
Four Pixels Studio <121856163+fourpixels-studio@users.noreply.github.com>
2025-03-07 22:48:33 +0300

    Added post testimonial functionality

--- frontend/views.py
+++ frontend/views.py
@@ -1,8 +1,9 @@
 from django.db.models import Count
+from .forms import TestimonialForm
+from django.contrib import messages
 from django.shortcuts import render
 from .models import Testimonial, HomePage
 from events.models import Event, EventCategory
-
 
 def index(request):
     category = request.GET.get('category')
@@ -37,3 +38,34 @@ def about(request):
         "title_tag": "About",
     }
     return render(request, "about.html", context)
+
+
+
+def submit_testimonial(request):
+    meta_description = str("Share your experience with us! Submit a testimonial and let others know about your thoughts and feedback.")
+    meta_keywords = str("testimonials, customer reviews, feedback, user experience, submit testimonial")
+    if request.method == 'POST':
+        testimonial_form = TestimonialForm(request.POST, request.FILES)
+        if testimonial_form.is_valid():
+            testimonial_form.save()
+            message = str("We appreciate you taking the time to share your experience. Your testimonial has been successfully submitted and will be published on our website. We value your feedback and thank you for your support!")
+            messages.success(request, message)
+            context = {
+                "meta_keywords": meta_keywords,
+                "meta_description": meta_description,
+                'testimonial_form': TestimonialForm(),
+                "title_tag": "Thank You For Sharing Your Experience With Us!",
+            }
+            return render(request, 'submit_testimonial.html', context)
+        else:
+            for field, errors in testimonial_form.errors.items():
+                for error in errors:
+                    messages.error(request, error)
+            testimonial_form = TestimonialForm()
+    context = {
+        "meta_keywords": meta_keywords,
+        "title_tag": "Submit a Testimonial",
+        "meta_description": meta_description,
+        'testimonial_form': TestimonialForm(),
+    }
+    return render(request, 'submit_testimonial.html', context)
